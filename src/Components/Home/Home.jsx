a6a5da7c2d1962f2f3b73c11571100f0ec997741
Lucas Prat <lucasprat.cn@gmail.com>
2023-07-31 01:39:12 -0300

    finish burguer menu and dropbox

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,12 +5,12 @@ import styles from './home.module.css'
 const Home = () => {
 	return (
 		<>
-		<div className={styles.container}>
-			<div className={styles.containerText} >
-				<h1>New season arrivals</h1>
-			</div>
-		</div>
-		<Products/>
+			<main className={styles.container}>
+				<div className={styles.containerText} >
+					<h1>New season arrivals</h1>
+				</div>
+			</main>
+			<Products />
 		</>
 	)
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import styles from './navbar.module.css'
 import { Link, NavLink, useNavigate, useLocation } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logOut } from '../../redux/auth/thunks';
-import { Button, Alert } from '../../Components/Shared/index';
+import { Button, Alert, Loader } from '../../Components/Shared/index';
 
 
 
@@ -30,89 +30,141 @@ const Navbar = () => {
   };
 
   return (
-    <>
-      <Alert show={showAlert} setShow={setShowAlert}>{childrenAlert}</Alert>
-      <header className={styles.container}>
-        <NavLink to="/"><h1> AMBAR </h1></NavLink>
-        <div className={styles.navbarCollapse}>
-          <ul>
-            <li className={styles.navItem}>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li className={styles.navItem}>
-              <NavLink to="/products">Products</NavLink>
-            </li>
-            <li className={styles.navItem}>
-              <NavLink to="/about">About</NavLink>
-            </li>
-            <li className={styles.navItem}>
-              <NavLink to="/contact">Contact</NavLink>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.navbarLogin}>
-          {
-            auth.token ? (
-              <div className={styles.navbarButtons}>
-                <Link to={`/profile/${auth.user.firebaseUid}`}>
-                  <Button className={styles.navButton}>
-                    <i className="fa-solid fa-user"></i> {nameCap}
-                  </Button>
-                </Link>
-                <Link to="/cart">
-                  <Button className={styles.navButton}>
-                    <i className="fa-solid fa-shopping-cart"></i>  Cart ({cart.length})
-                  </Button>
-                </Link>
-
-                {auth.user.isAdmin &&
+    auth?.user?.isLoading ? <Loader show={auth?.user?.isLoading} /> :
+      <>
+        <Alert show={showAlert} setShow={setShowAlert}>{childrenAlert}</Alert>
+        <header>
+          <div className={styles.container}>
+            <Link to="/"><h1> AMBAR </h1></Link>
+            <div className={styles.navbarCollapse}>
+              <ul>
+                <li className={styles.navItem}>
+                  <NavLink to="/">Home</NavLink>
+                </li>
+                <li className={styles.navItem}>
+                  <NavLink to="/products">Products</NavLink>
+                </li>
+                <li className={styles.navItem}>
+                  <NavLink to="/about">About</NavLink>
+                </li>
+                <li className={styles.navItem}>
+                  <NavLink to="/contact">Contact</NavLink>
+                </li>
+              </ul>
+            </div>
+            <div className={styles.navbarLogin}>
+              {
+                auth.token ? (
                   <>
-                    <div className={styles.dropdown}>
-                      <Button className={styles.dropButton}>
-                        <i className="fa-solid fa-clipboard-list"></i>  Menu
+                    <div className={styles.navbarButtons}>
+                      <Link to={`/profile/${auth.user.firebaseUid}`}>
+                        <Button className={styles.navButton}>
+                          <i className="fa-solid fa-user"></i> {nameCap}
+                        </Button>
+                      </Link>
+                      <Link to="/cart">
+                        <Button className={styles.navButton}>
+                          <i className="fa-solid fa-shopping-cart"></i>  Cart ({cart.length})
+                        </Button>
+                      </Link>
+
+                      {auth.user.isAdmin &&
+                        <div className={styles.dropAdmin}>
+                          <Button className={styles.dropAdminButton}>
+                            <i className="fa-solid fa-clipboard-list"></i>  Admin Menu
+                          </Button>
+                          <div className={styles.dropAdminContent}>
+                            <Link to="/abmorders">
+                              <i className="fa-solid fa-clipboard-list"></i>  ABM Orders
+                            </Link>
+                            <Link to="/abmproducts">
+                              <i className="fa-solid fa-shopping-cart"></i> ABM Products
+                            </Link>
+                          </div>
+                        </div>
+                      }
+
+                      <Button onClick={() => handleExit()} className={styles.navButton}>
+                        <i className="fa fa-sign-in me-1"></i>  Exit
                       </Button>
+                    </div>
+
+                    {/* dropdown mobile */}
+                    <div className={styles.dropdown}>
+                      <button className={styles.burgerButton}>
+                        <i className="fa-solid fa-bars fa-2xl"></i>
+                      </button>
                       <div className={styles.dropdownContent}>
-                        <Link to="/abmorders">
-                          <i className="fa-solid fa-clipboard-list"></i>  ABM Orders
+                        <Link to={`/profile/${auth.user.firebaseUid}`}>
+                          <i className="fa-solid fa-user"></i> {nameCap}
                         </Link>
-                        <Link to="/abmproducts">
-                          <i className="fa-solid fa-clipboard-list"></i> ABM Products
+                        <Link to="/cart">
+                          <i className="fa-solid fa-shopping-cart"></i>  Cart ({cart.length})
+                        </Link>
+
+                        {auth.user.isAdmin &&
+                          <div className={styles.dropAdmin}>
+                            <Link className={styles.dropAdminButton}>
+                              <i className="fa-solid fa-clipboard-list"></i>  Admin Menu
+                            </Link>
+                            <div className={styles.dropAdminMobileContent}>
+                              <Link to="/abmorders">
+                                <i className="fa-solid fa-clipboard-list"></i>  Orders
+                              </Link>
+                              <Link to="/abmproducts">
+                                <i className="fa-solid fa-shopping-cart"></i>  Products
+                              </Link>
+                            </div>
+                          </div>
+                        }
+
+                        <Link onClick={() => handleExit()}>
+                          <i className="fa fa-sign-in me-1"></i>  Exit
                         </Link>
                       </div>
                     </div>
                   </>
-                }
+                ) : (
+                  location.pathname === '/signin' ? (
+                    <div className={styles.navbarSign}>
+                      <Link to="/signup">
+                        <Button className={styles.navButton}>
+                          <i className="fa-solid fa-user"></i> Sign Up
+                        </Button>
+                      </Link>
+                    </div>
+                  ) : (
+                    <div className={styles.navbarSign}>
+                      <Link to="/signin">
+                        <Button className={styles.navButton}>
+                          <i className="fa-solid fa-user"></i> Sign In
+                        </Button>
+                      </Link>
+                    </div>
+                  )
+                )
+              }
+            </div>
 
-                <Button onClick={() => handleExit()} className={styles.navButton}>
-                  <i className="fa fa-sign-in me-1"></i>  Exit
-                </Button>
-              </div>
-            ) : (
-              location.pathname === '/signin' ? (
-                <div className={styles.navbarButtons}>
-                  <Link to="/signup">
-                    <Button className={styles.navButton}>
-                      <i className="fa-solid fa-user"></i> Sign Up
-                    </Button>
-                  </Link>
-                </div>
-              ) : (
-                <div className={styles.navbarButtons}>
-                  <Link to="/signin">
-                    <Button className={styles.navButton}>
-                      <i className="fa-solid fa-user"></i> Sign In
-                    </Button>
-                  </Link>
-                </div>
-              )
-            )
-          }
-        </div>
-        <button className={styles.burgerButton}>
-          <i className="fa-solid fa-bars fa-2xl"></i>
-        </button>
-      </header>
-    </>
+          </div>
+          <div className={styles.navbarMobile}>
+            <ul>
+              <li className={styles.navItem}>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li className={styles.navItem}>
+                <NavLink to="/products">Products</NavLink>
+              </li>
+              <li className={styles.navItem}>
+                <NavLink to="/about">About</NavLink>
+              </li>
+              <li className={styles.navItem}>
+                <NavLink to="/contact">Contact</NavLink>
+              </li>
+            </ul>
+          </div>
+        </header>
+      </>
   )
 }
 
